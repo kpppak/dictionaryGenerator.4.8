f8e1e10f9ab9fb1b43c0c8d59bf70186d56cdd59
Ekaterina Pak <ppakshome016@gmail.com>
2022-03-05 22:09:27 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        final int MIN_FREQUENCY = 1;
+        final String TEXT =
+                "Meet my family." + "There are five of us – my parents," + " my elder brother, my baby sister and me." +
+                        "First, meet my mum and dad, Jane and Michael." +
+                        "My mum enjoys reading and my dad enjoys" + "playing chess with my brother Ken." +
+                        "My mum is slim and rather tall." + "She has long red hair and big brown eyes." +
+                        "She has a very pleasant smile and a soft voice. My mother is very kind and understanding.";
+        List<String> dictionary = words.apply(TEXT).stream()
+                .filter(e -> Collections.frequency(words.apply(TEXT), e) >= MIN_FREQUENCY)
+                .collect(Collectors.toList());
+
+        dictionary.stream()
+                .forEach(System.out::println);
+        }
+
+    public static Function<String, List<String>> words = x -> Arrays.stream(x.split("\\b"))
+            .map(String::toUpperCase)
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
+}
+
+
+
